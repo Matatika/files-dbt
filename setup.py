a0438dd3675fab0bfc6b6243541606ed9be1bc84
danielpdwalker <danielpdwalker@gmail.com>
2022-01-26 15:24:07 +0000

    Inital files-dbt commit with new env vars in profiles

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="files-dbt",
+    version="0.3",
+    description="Matatika project files for dbt",
+    packages=find_packages(),
+    package_data={
+        "bundle": [
+            "transform/models/.gitkeep",
+            "transform/profile/profiles.yml",
+            "transform/.gitignore",
+            "transform/dbt_project.yml",
+        ]
+    },
+)
